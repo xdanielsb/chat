5dde4159947b3a9803f3819fee27bfbc7856381b
Daniel Santos <dfsantosbu@unal.edu.co>
2018-01-26 16:21:43 -0500

    Render main page.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_socketio import SocketIO, send, emit
 
 app = Flask(__name__)
@@ -8,7 +8,10 @@ socketio = SocketIO(app)
 @socketio.on('message')
 def handlem(msg):
     send(msg, broadcast=True, include_self=False)
-    
+
+@app.route('/')
+def homepage():
+    return render_template('chat.html')
 
 if __name__ == '__main__':
     socketio.run(app)
