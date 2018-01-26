8a26b67f1b2453aa20896067543db84cbb63513c
Daniel Santos <dfsantosbu@unal.edu.co>
2018-01-25 21:18:25 -0500

    Add handler for incoming messages.

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mys3cr31'
 socketio = SocketIO(app)
 
+@socketio.on('message')
+def handlem(msg):
+    print("Message {}".format(msg))
+    send(msg, broadcast=True)
+
 if __name__ == '__main__':
     socketio.run(app)
-    
